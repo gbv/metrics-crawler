0df7b8e0ed62bb7c870b4b688da6923507e0fae3
Julius Stropel <julius.stropel@gbv.de>
2019-01-07 09:03:04 +0100

    modified reset logs method

--- agents/rem_logs.js
+++ agents/rem_logs.js
@@ -1,19 +1,29 @@
 const { exec } = require('child_process');
 
-var config = require('./config.json');
+// var config = require('./config.json');
 
-var forever_log_path = config.forever_log_path;
+// var forever_log_path = config.forever_log_path;
 
-exec('rm -rf '+forever_log_path+'/*.log', (err, stdout, stderr) => {
+// exec('rm -rf '+forever_log_path+'/*.log', (err, stdout, stderr) => {
+
+	// if(err){
+		// console.log(err);
+	// }
+
+	// exec('ls '+forever_log_path, (err, stdout, stderr) => {
+		
+		// console.log(stdout);
+		
+	// });
+
+// });
+
+exec('forever cleanlogs', (err, stdout, stderr) => {
 
 	if(err){
 		console.log(err);
+	}else{
+		console.log("Forever logs cleaned.");
 	}
-
-	exec('ls '+forever_log_path, (err, stdout, stderr) => {
-		
-		console.log(stdout);
-		
-	});
-
+	
 });
